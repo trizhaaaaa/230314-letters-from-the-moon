2d4fae3e25326102f9ab605890e6113edb5ff3e7
IT <IT@FFC-00105-PC>
2023-03-10 14:44:17 +0800

    Add project files.

---
+++ jess/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace jess
+{
+    partial class moonMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tt = new System.Windows.Forms.ToolTip(this.components);
+            this.moonExit = new System.Windows.Forms.PictureBox();
+            this.moonInfo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.moonExit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moonInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // moonExit
+            // 
+            this.moonExit.Image = global::jess.Properties.Resources.x;
+            this.moonExit.Location = new System.Drawing.Point(495, 113);
+            this.moonExit.Name = "moonExit";
+            this.moonExit.Size = new System.Drawing.Size(50, 50);
+            this.moonExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moonExit.TabIndex = 2;
+            this.moonExit.TabStop = false;
+            this.moonExit.Click += new System.EventHandler(this.moonExit_Click);
+            // 
+            // moonInfo
+            // 
+            this.moonInfo.Image = global::jess.Properties.Resources.star;
+            this.moonInfo.Location = new System.Drawing.Point(430, 113);
+            this.moonInfo.Name = "moonInfo";
+            this.moonInfo.Size = new System.Drawing.Size(50, 50);
+            this.moonInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moonInfo.TabIndex = 1;
+            this.moonInfo.TabStop = false;
+            this.moonInfo.Click += new System.EventHandler(this.moonInfo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::jess.Properties.Resources.moon;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 169);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // moonMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 700);
+            this.Controls.Add(this.moonExit);
+            this.Controls.Add(this.moonInfo);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "moonMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.moonExit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moonInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox moonInfo;
+        private System.Windows.Forms.PictureBox moonExit;
+        private System.Windows.Forms.ToolTip tt;
+    }
+}
+
---
+++ jess/popupInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jess
+{
+    public partial class popupInfo : Form
+    {
+        public popupInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            moonMain mm = new moonMain();
+            mm.Show();
+        }
+
+        private void popupInfo_Load(object sender, EventArgs e)
+        {
+
+            this.BackColor = Color.SaddleBrown;
+            this.TransparencyKey = Color.SaddleBrown;
+
+            var it1Loc = infoText1.PointToScreen(Point.Empty); 
+            infoText1.Parent = bg1;
+            infoText1.Location = bg1.PointToClient(it1Loc);
+            infoText1.BackColor = Color.Transparent;
+
+            var it2Loc = infoText2.PointToScreen(Point.Empty);
+            infoText2.Parent = bg1;
+            infoText2.Location = bg1.PointToClient(it2Loc);
+            infoText2.BackColor = Color.Transparent;
+
+            var it3Loc = infoText3.PointToScreen(Point.Empty);
+            infoText3.Parent = bg1;
+            infoText3.Location = bg1.PointToClient(it3Loc);
+            infoText3.BackColor = Color.Transparent;
+
+            var it4Loc = infoText4.PointToScreen(Point.Empty);
+            infoText4.Parent = bg1;
+            infoText4.Location = bg1.PointToClient(it4Loc);
+            infoText4.BackColor = Color.Transparent;
+
+            var it5Loc = infoText5.PointToScreen(Point.Empty);
+            infoText5.Parent = bg2;
+            infoText5.Location = bg2.PointToClient(it5Loc);
+            infoText5.BackColor = Color.Transparent;
+
+            var it6Loc = infoText6.PointToScreen(Point.Empty);
+            infoText6.Parent = bg2;
+            infoText6.Location = bg2.PointToClient(it6Loc);
+            infoText6.BackColor = Color.Transparent;
+
+
+            tt.SetToolTip(this.back, "◄ to the moon");
+
+        }
+    }
+}
