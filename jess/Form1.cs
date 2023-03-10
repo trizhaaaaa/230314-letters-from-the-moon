f0e9cf0ec8a3ce944496a71b3761952fcea3acd8
IT <IT@FFC-00105-PC>
2023-03-10 14:44:17 +0800

    main moon and info popup form

---
+++ jess/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jess
+{
+    public partial class moonMain : Form
+    {
+        public moonMain()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.SaddleBrown;
+            this.TransparencyKey = Color.SaddleBrown;
+
+            tt.SetToolTip(this.moonInfo, "Sagot sa \"Anong gagawin ko dito, beh???\"");
+            tt.SetToolTip(this.moonExit, "Babyeeee! ♥");
+        }
+
+        private void moonExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void moonInfo_Click(object sender, EventArgs e)
+        {
+            popupInfo popup = new popupInfo();
+            popup.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ jess/popupInfo.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace jess
+{
+    partial class popupInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.back = new System.Windows.Forms.PictureBox();
+            this.bg1 = new System.Windows.Forms.PictureBox();
+            this.tt = new System.Windows.Forms.ToolTip(this.components);
+            this.infoText1 = new System.Windows.Forms.Label();
+            this.bg2 = new System.Windows.Forms.PictureBox();
+            this.infoText2 = new System.Windows.Forms.Label();
+            this.infoText3 = new System.Windows.Forms.Label();
+            this.infoText4 = new System.Windows.Forms.Label();
+            this.infoText5 = new System.Windows.Forms.Label();
+            this.infoText6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // back
+            // 
+            this.back.Image = global::jess.Properties.Resources.red;
+            this.back.Location = new System.Drawing.Point(528, 72);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(60, 50);
+            this.back.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.back.TabIndex = 2;
+            this.back.TabStop = false;
+            this.back.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // bg1
+            // 
+            this.bg1.Image = global::jess.Properties.Resources.title_white;
+            this.bg1.Location = new System.Drawing.Point(12, 128);
+            this.bg1.Name = "bg1";
+            this.bg1.Size = new System.Drawing.Size(576, 191);
+            this.bg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bg1.TabIndex = 0;
+            this.bg1.TabStop = false;
+            // 
+            // infoText1
+            // 
+            this.infoText1.BackColor = System.Drawing.SystemColors.Control;
+            this.infoText1.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
+            this.infoText1.Location = new System.Drawing.Point(67, 161);
+            this.infoText1.Name = "infoText1";
+            this.infoText1.Size = new System.Drawing.Size(464, 30);
+            this.infoText1.TabIndex = 4;
+            this.infoText1.Text = "Who was the first woman on the Moon?";
+            // 
+            // bg2
+            // 
+            this.bg2.Image = global::jess.Properties.Resources.title_black;
+            this.bg2.Location = new System.Drawing.Point(12, 325);
+            this.bg2.Name = "bg2";
+            this.bg2.Size = new System.Drawing.Size(576, 191);
+            this.bg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bg2.TabIndex = 5;
+            this.bg2.TabStop = false;
+            // 
+            // infoText2
+            // 
+            this.infoText2.BackColor = System.Drawing.SystemColors.Control;
+            this.infoText2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
+            this.infoText2.Location = new System.Drawing.Point(71, 191);
+            this.infoText2.Name = "infoText2";
+            this.infoText2.Size = new System.Drawing.Size(477, 42);
+            this.infoText2.TabIndex = 4;
+            this.infoText2.Text = "\"Unfortunately, no woman has visited the Moon.  At the time of Apollo (1969-72), " +
+    "there were no women in the U.S. astronaut corps.\"";
+            // 
+            // infoText3
+            // 
+            this.infoText3.BackColor = System.Drawing.SystemColors.Control;
+            this.infoText3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
+            this.infoText3.Location = new System.Drawing.Point(68, 243);
+            this.infoText3.Name = "infoText3";
+            this.infoText3.Size = new System.Drawing.Size(480, 31);
+            this.infoText3.TabIndex = 4;
+            this.infoText3.Text = "https://sservi.nasa.gov/?question=first-woman-on-the-moon#:~:text=Who%20was%20the" +
+    "%20first%20woman,in%20the%20U.S.%20astronaut%20corps.";
+            // 
+            // infoText4
+            // 
+            this.infoText4.BackColor = System.Drawing.SystemColors.Control;
+            this.infoText4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
+            this.infoText4.Location = new System.Drawing.Point(299, 284);
+            this.infoText4.Name = "infoText4";
+            this.infoText4.Size = new System.Drawing.Size(232, 23);
+            this.infoText4.TabIndex = 4;
+            this.infoText4.Text = "(sa 2024 pa raw haha - NASA Artemis)";
+            this.infoText4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // infoText5
+            // 
+            this.infoText5.BackColor = System.Drawing.Color.Black;
+            this.infoText5.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.infoText5.Location = new System.Drawing.Point(66, 371);
+            this.infoText5.Name = "infoText5";
+            this.infoText5.Size = new System.Drawing.Size(462, 64);
+            this.infoText5.TabIndex = 6;
+            this.infoText5.Text = "With that said,,,, napagdesisyunan naming ikaw na ang mauuna !! HAHAHAH";
+            // 
+            // infoText6
+            // 
+            this.infoText6.BackColor = System.Drawing.Color.Black;
+            this.infoText6.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoText6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.infoText6.Location = new System.Drawing.Point(66, 435);
+            this.infoText6.Name = "infoText6";
+            this.infoText6.Size = new System.Drawing.Size(462, 43);
+            this.infoText6.TabIndex = 6;
+            this.infoText6.Text = "Have fun exploring this moon! ♥";
+            this.infoText6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // popupInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 571);
+            this.Controls.Add(this.infoText6);
+            this.Controls.Add(this.infoText5);
+            this.Controls.Add(this.bg2);
+            this.Controls.Add(this.infoText3);
+            this.Controls.Add(this.infoText4);
+            this.Controls.Add(this.infoText2);
+            this.Controls.Add(this.infoText1);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.bg1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "popupInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "popupInfo";
+            this.Load += new System.EventHandler(this.popupInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.back)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bg1;
+        private System.Windows.Forms.PictureBox back;
+        private System.Windows.Forms.ToolTip tt;
+        private System.Windows.Forms.Label infoText1;
+        private System.Windows.Forms.PictureBox bg2;
+        private System.Windows.Forms.Label infoText2;
+        private System.Windows.Forms.Label infoText3;
+        private System.Windows.Forms.Label infoText4;
+        private System.Windows.Forms.Label infoText5;
+        private System.Windows.Forms.Label infoText6;
+    }
+}
